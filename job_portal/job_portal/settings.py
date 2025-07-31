0b0a676752c7e5601b31665681198f5e4a388bcc
unknown <digilinkbd@gmail.com>
2025-07-31 18:35:26 +0600

    deployment setting

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -78,15 +78,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Move this here (2nd position)
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
 ]
+
 
 ROOT_URLCONF = 'job_portal.urls'
 
